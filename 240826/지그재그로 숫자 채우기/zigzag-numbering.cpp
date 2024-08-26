07fccc63eca6a521fab3922343c6eda9ce726ff9
Dewmo123 <109888343+Dewmo123@users.noreply.github.com>
2024-08-26 20:06:11 +0900

    Title: 지그재그로 숫자 채우기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240826/지그재그로 숫자 채우기/zigzag-numbering.cpp
+++ 240826/지그재그로 숫자 채우기/zigzag-numbering.cpp
@@ -9,14 +9,14 @@ int main() {
         arr[i] = new int[m];
     //0,0 1,0 2,0 3,0 3,1 2,1 1,1 0,1 0,2
     int cnt = 0;
-    for (int y = 0;y < n;y++) {
+    for (int y = 0;y < m;y++) {
         if (y % 2 == 0) {
-            for (int x = 0;x < m;x++) {
+            for (int x = 0;x < n;x++) {
                 arr[x][y] = cnt++;
             }
         }
         else {
-            for (int x = m - 1;x >= 0;x--) {
+            for (int x = n - 1;x >= 0;x--) {
                 arr[x][y] = cnt++;
             }
         }
