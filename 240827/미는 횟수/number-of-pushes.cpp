06decc0d7fe25af4f7fa7529c27bbefca5584abd
Dewmo123 <109888343+Dewmo123@users.noreply.github.com>
2024-08-27 19:15:10 +0900

    Title: 미는 횟수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240827/미는 횟수/number-of-pushes.cpp
+++ 240827/미는 횟수/number-of-pushes.cpp
@@ -3,21 +3,27 @@
 using namespace std;
 
 int main() {
-    string s, s2;
-    cin >> s >> s2;
-    int n = 0;
-    for (int i = 0;i < s.size();i++) {
-        if (s[i] == s2[0]) break;
-        n++;
-    }
-    for (int i = 0;i < n;i++) {
-        char flag = s[0];
-        for (int i = 1;i < s.size();i++) {
-            s[i - 1] = s[i];
-        }
-        s[s.size() - 1] = flag;
-    }
-    if (s != s2)cout << -1;
-    else cout << n;
-    return 0;
+	string s, s2;
+	cin >> s >> s2;
+	int n = 0;
+	while (true)
+	{
+		if (s == s2)break;
+		if (n > s.size()) {
+			n = -1;
+			break;
+		}
+		char flag = s[s.size()-1];
+		char flag2 = s[0];
+		for (int i = 0;i < s.size()-1;i++) {
+			char cha = s[i + 1];
+ 			s[i+1] = flag2;
+			flag2 = cha;
+		}
+		n++;
+		s[0] = flag;
+	}
+	if (s != s2)cout << -1;
+	else cout << n;
+	return 0;
 }
